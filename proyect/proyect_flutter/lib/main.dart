d3359bd1f3222e778a6b1ea108398f8198054bf3
SerDieFer <145585172+SerDieFer@users.noreply.github.com>
2024-07-18 14:36:46 +0200

    bonito y barato, pero asqueroso

--- proyect/proyect_flutter/lib/main.dart
+++ proyect/proyect_flutter/lib/main.dart
@@ -5,34 +5,36 @@ import 'package:serverpod_flutter/serverpod_flutter.dart';
 import 'log-reg/login.dart';
 import 'toDoList/to_do_list.dart';
 
-// Sets up a singleton client object that can be used to talk to the server from
-// anywhere in our app. The client is generated from your server code.
-// The client is set up to connect to a Serverpod running on a local server on
-// the default port. You will need to modify this to connect to staging or
-// production servers.
+// SETS UP A SINGLETON CLIENT OBJECT THAT CAN BE USED TO TALK TO THE SERVER FROM
+// ANYWHERE IN OUR APP. THE CLIENT IS GENERATED FROM YOUR SERVER CODE.
+// THE CLIENT IS SET UP TO CONNECT TO A SERVERPOD RUNNING ON A LOCAL SERVER ON
+// THE DEFAULT PORT. YOU WILL NEED TO MODIFY THIS TO CONNECT TO STAGING OR
+// PRODUCTION SERVERS.
 Client client = Client('http://$localhost:8080/')
   ..connectivityMonitor = FlutterConnectivityMonitor();
 
-void main() {
-  runApp(MaterialApp(
-    routes: {
-      // This will set our basic routes for the proyect.
-      // Later we will have to work with dinamic routes.
-      '/' : (context) => Login(client: client,),
-      '/register' : (context) => Register(client: client,),
-      '/todolist' : (context) {
-        // We will need to extract arguments to set our ToDoList.
-        // ModalRoute get the arguments and return a kind of object
-        final args = ModalRoute.of(context)?.settings.arguments;
-        // If in the args we get an integer (userID), we will adapt our ToDoList
-        if (args is int) {
-          return ToDoList(client: client, userId: args);
+void main() 
+{
+  // THIS WILL SET OUR BASIC ROUTES FOR THE PROYECT.
+  // LATER WE WILL HAVE TO WORK WITH DINAMIC ROUTES.
+  runApp(MaterialApp( routes: { '/' : (context) => Login(client: client, ),
+                                '/register' : (context) => Register(client: client, ),
+
+                                // WE WILL NEED TO EXTRACT ARGUMENTS TO SET OUR TODOLIST.
+                                // MODALROUTE GET THE ARGUMENTS AND RETURN A KIND OF OBJECT
+                                '/todolist' : (context) { final args = ModalRoute.of(context)?.settings.arguments;
+
+                                                          // IF IN THE ARGS WE GET AN INTEGER (USERID), WE WILL ADAPT OUR TODOLIST
+                                                          if (args is int) 
+                                                          {
+                                                            return ToDoList(client: client, userId: args);
+                                                          }
+                                                          // TODO: WE NEED TO CREATE AN ERROR POPUP TO CALL HERE.
+                                                          return ToDoList(client: client, userId: 1);
         }
-        // TODO: We need to create an error popup to call here.
-        return ToDoList(client: client, userId: 1);
-      }
-    },
-  ));
+      },
+    ) 
+  );
 } 
 
 
