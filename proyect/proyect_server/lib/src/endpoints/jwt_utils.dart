d872c89b156361c961fb356e76976a968117edcb
Santiago Lorenzano <enderwigim@gmail.com>
2024-07-23 14:04:57 +0200

    JwtUtils created

---
+++ proyect/proyect_server/lib/src/endpoints/jwt_utils.dart
@@ -0,0 +1,21 @@
+
+import 'package:proyect_server/src/models/environment/env.dart';
+import 'package:serverpod/serverpod.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+
+class JwtUtils {
+  static final String _secretKey = 'your_secret_key';
+
+  static String generateToken(Map<String, dynamic> payload) {
+    final jwt = JWT(payload);
+    return jwt.sign(SecretKey(_secretKey));
+  }
+
+  static JWT? verifyToken(String token) {
+    try {
+      return JWT.verify(token, SecretKey(_secretKey));
+    } catch (e) {
+      return null;
+    }
+  }
+}
